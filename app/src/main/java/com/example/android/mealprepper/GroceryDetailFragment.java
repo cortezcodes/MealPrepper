c9ccc60601202d11e63e026dc012aaaaec86fc17
CortezCodes <cortez.mccrary.codes@gmail.com>
2020-03-28 23:35:47 -0700

    Add Item
    - Add Item to database
    - Changed Units from EditText to Spinner View
    - add singular and plural units.

--- app/src/main/java/com/example/android/mealprepper/GroceryDetailFragment.java
+++ app/src/main/java/com/example/android/mealprepper/GroceryDetailFragment.java
@@ -1,12 +1,19 @@
 package com.example.android.mealprepper;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,10 +23,14 @@ import androidx.lifecycle.ViewModelProvider;
 /**
  * Fragment used to add and edit Grocery List Items
  */
-public class GroceryDetailFragment extends Fragment {
-    private EditText itemEditText, quantityEditText, unitEditText;
+public class GroceryDetailFragment extends Fragment{
+    private EditText itemEditText, quantityEditText;
+    private Spinner unitSpinner;
     private Button addEditButton, cancelButton;
     private GroceryViewModel mGroceryViewModel;
+    private ArrayAdapter<String> spinnerAdapter;
+    private List<String> spinnerOptions;
+
 
     @Nullable
     @Override
@@ -29,28 +40,102 @@ public class GroceryDetailFragment extends Fragment {
 
         this.itemEditText = view.findViewById(R.id.edit_text_item_name);
         this.quantityEditText = view.findViewById(R.id.edit_text_quantity);
-        this.unitEditText = view.findViewById(R.id.edit_text_units);
+        this.unitSpinner = view.findViewById(R.id.unit_spinner);
         this.addEditButton = view.findViewById(R.id.button_add_edit);
         this.cancelButton = view.findViewById(R.id.button_cancel);
+
+        //Setup unit spinner
+        spinnerOptions = new ArrayList<>();
+        spinnerOptions.addAll(GroceryUtil.getSingleUnits());
+        spinnerAdapter = new ArrayAdapter<String>(this.getContext(), R.layout.support_simple_spinner_dropdown_item, spinnerOptions);
+        spinnerAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        this.unitSpinner.setAdapter(spinnerAdapter);
 
         //Get a new or existing ViewModel form the ViewModelProvider
         ViewModelProvider.Factory factory = new ViewModelProvider.AndroidViewModelFactory(this.getActivity().getApplication());
         this.mGroceryViewModel = new ViewModelProvider(this, factory).get(GroceryViewModel.class);
 
-        //TODO Add Grocery Functionality
+        //onChangeListener for quantity EditText to change spinner from singular to plural if
+        this.quantityEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(s.length() != 0) {
+                    if (Integer.parseInt(s.toString()) > 1) {
+                        spinnerOptionControl(true);
+                        //spinnerOptions.clear();
+                       // spinnerOptions.addAll(GroceryUtil.getPluralUnits());
+
+                    } else {
+                        spinnerOptionControl(false);
+                       //spinnerOptions.clear();
+                        //spinnerOptions.addAll(GroceryUtil.getSingleUnits());
+                    }
+                }
+            }
+        });
+        // add/edit button onClickListener that updates the database
         addEditButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getContext(), "Add item", Toast.LENGTH_SHORT).show();
+                String item = itemEditText.getText().toString();
+                String quantity = quantityEditText.getText().toString();
+                String units = unitSpinner.getSelectedItem().toString();
+
+                if(item.matches("")){
+                    Toast.makeText(getContext(), "Please add a Item name", Toast.LENGTH_SHORT).show();
+                }else if(quantity.matches("") || quantity.matches("0")) {
+                    Toast.makeText(getContext(), "Please fill in a quantity", Toast.LENGTH_SHORT).show();
+                } else{
+                    mGroceryViewModel.insert(new Grocery(item, Integer.parseInt(quantity), units));
+                    exitDetailedView();
+                }
+
             }
         });
 
         cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getActivity().getSupportFragmentManager().popBackStack();
+                exitDetailedView();
             }
         });
         return view;
+    }
+
+    private void exitDetailedView(){
+        getActivity().getSupportFragmentManager().popBackStack();
+    }
+
+    /**
+     * Dynamically updates the Unit Spinner so that when QuantityEditText view has a value greater
+     * than 1 then it makes all the options plural.
+     * @param isPlural if this is true then the spinner will set plural values
+     */
+    private void spinnerOptionControl(boolean isPlural){
+        if(isPlural){
+            spinnerOptions.clear();
+            spinnerOptions.addAll(GroceryUtil.getPluralUnits());
+            spinnerAdapter.notifyDataSetChanged();
+            //spinnerAdapter.clear();
+           // spinnerAdapter.addAll(spinnerOptions);
+
+        }else{
+            spinnerOptions.clear();
+            spinnerOptions.addAll(GroceryUtil.getSingleUnits());
+            spinnerAdapter.notifyDataSetChanged();
+            //spinnerAdapter.clear();
+            //spinnerAdapter.addAll(spinnerOptions);
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/android/mealprepper/GroceryUtil.java
@@ -0,0 +1,29 @@
+package com.example.android.mealprepper;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class GroceryUtil {
+    private static final List<String> singleUnits = new ArrayList<>(Arrays.asList("", "lb", "oz", "gal", "qt",
+            "pt", "c.","fl. oz.", "Tbsp.", "tsp.", "mL.", "l.",
+            "g.", "kg.", "doz.", "box", "roll", "bottle", "can",
+            "bunch", "package", "pack", "loaf", "other"));
+
+    private static final List<String>  pluralUnits = new ArrayList<>(Arrays.asList("", "lb", "oz", "gal", "qt",
+            "pt", "c.","fl. oz.", "Tbsp.", "tsp.", "mL.", "l.",
+            "g.", "kg.", "doz.", "boxes", "rolls", "bottles", "cans",
+            "bunches", "packages", "packs", "loaves", "other"));
+
+    public static String pluralize(int position){
+        return pluralUnits.get(position);
+    }
+
+    public static List<String> getSingleUnits() {
+        return singleUnits;
+    }
+
+    public static List<String> getPluralUnits() {
+        return pluralUnits;
+    }
+}
